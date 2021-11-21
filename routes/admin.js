77ccb6ad4af92c3d81068c00923b36a7b66f14b6
Jakub Spacil <jakubspacil@gmail.com>
2021-11-21 20:33:31 +0100

    error

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const { isLoggedIn, isAdmin } = require('../utils/middleware');
 //show all cards in the DB
 router.get('/admin/listallcards', isLoggedIn, isAdmin, catchAsync(async (req, res, next) => {
     const allCards = await Card.find({}); 
-    res.render('listAllCards', {cards: allCards});
+    res.render('listallcards', {cards: allCards});
 }))
 
 module.exports = router;
