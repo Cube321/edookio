bf11d17c5a380077a8f69baf6d7650f73074c984
Jakub Spacil <jakubspacil@gmail.com>
2023-11-14 20:54:35 +0100

    same

--- public/js/card.js
+++ public/js/card.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     })
 
     var pageB = document.getElementById('pageB');
-    if (pageB.scrollHeight > pageB.clientHeight) {
+    var innerCardDiv = document.getElementById('inner-card-div');
+    if (innerCardDiv.scrollHeight > innerCardDiv.clientHeight) {
         pageB.classList.add('text-gradient');
     }
 
