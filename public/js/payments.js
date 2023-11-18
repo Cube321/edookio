9af75f384e1ca66152b13d355bd8e635fed0f8a6
Jakub Spacil <jakubspacil@gmail.com>
2023-11-18 22:30:21 +0100

    stripe payments webhooks updated - removed created event

--- public/js/payments.js
+++ public/js/payments.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	const PUBLISHABLE_KEY = 'pk_test_51M7LRDAaRhuCsgZoSmUjyXiHbiKnXBc3g4YgwigzTCTfFpUMAB6NizhS3tL8WzxE9ICPRJl5Rzz6KiCJaxCVczwc00ZEs1F1zJ'
+	const PUBLISHABLE_KEY = 'pk_live_51M7LRDAaRhuCsgZolxnaCehFzl4FEpRIZkekLrpl73qisyaDz7Fo5fwlQ3Pfh5Kc14191BPy3k9zxVwBbgB1kNtJ00TFIm1DaS'
   
     const stripe = Stripe(
       PUBLISHABLE_KEY)
 
 	//Stripe payment
   //TEST DAILY
- 
+  /*
   const checkoutButtonDaily = $('#checkout-button-daily')
   
   checkoutButtonDaily.click(function () {
@@ -24,7 +24,7 @@ $(document).ready(function() {
       .then((result) => result.json())
       .then(({ sessionId }) => stripe.redirectToCheckout({ sessionId }))
   })
-  
+  */
   
 
   //Monthly Subscription
