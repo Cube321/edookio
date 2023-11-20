360d804ef6fb56004f8bf337bd890962911d86d5
Jakub Spacil <jakubspacil@gmail.com>
2023-11-20 18:43:17 +0100

    card design rebuild

--- public/js/card.js
+++ public/js/card.js
@@ -39,57 +39,49 @@ $(document).ready(function() {
             }
             if(data.user && data.user.isPremium && data.isCardSaved) {starFull = `<div id="save-star-div" name="${data.user.email}/${data.card._id}" class="save-star clicked" style="cursor:pointer"><i class="fas fa-star fa-lg"></i></div>`}
             if(data.user && data.user.isPremium && !data.isCardSaved) {starEmpty = `<div id="save-star-div" name="${data.user.email}/${data.card._id}" class="save-star" style="cursor:pointer"><i class="far fa-star fa-lg"></i></div>`}
-            if(data.user && !data.user.isPremium) {starActivateModal = `<div id="" class="save-star" style="cursor:pointer"><a href="#" data-bs-toggle="modal" data-bs-target="#savePremium"><i class="far fa-star fa-lg"></i></a></div>`}
-            if(!data.user) {starPlaceholder = `<div style="width:14px"></div>`} 
+            starActivateModal = `<div id="" class="save-star" style="cursor:pointer"><a href="#" data-bs-toggle="modal" data-bs-target="#savePremium"><i class="far fa-star fa-lg"></i></a></div>`
             //update card content
             $("#flip-card").empty();
             $("#flip-card").append(`
             <div class="border-grey flip-card-inner">
                 <div class="card-body flip-card-front pb-0">
-                <div class="front-main-content d-flex justify-content-center align-items-center">
-                    <div id="loaderA"></div>
-                    <div class="card-text m-4 text-center" id="pageA">${data.card.pageA}</div>
-                </div>
-                <div class="front-menu d-flex justify-content-between align-items-center">
-                    <a href=""></a>
-                    <div class="">
-                    <a href="#" class="btn btn-lg btn-outline-danger" id='btn-otocit'>Otočit</a>
+                    <div class="front-main-content d-flex justify-content-center align-items-center">
+                        <div id="loaderA"></div>
+                        <div class="card-text m-4 text-center" id="pageA">${data.card.pageA}</div>
                     </div>
-                    <a href=""></a>
-                </div>
                 </div>
                 <div class="card-body flip-card-back pb-0">
-                <div class="back-main-content d-flex justify-content-center align-items-center text-gradient">
-                    <div id="loaderB"></div>
-                    <div class="card-text m-4" id="pageB">${data.card.pageB}</div>
-                </div>
-                <div class="back-menu d-flex justify-content-between align-items-center">
-                    <div class="nezobrazovat-na-mobilu">
-                    ${starEmpty}
-                    ${starFull}
-                    ${starActivateModal}
-                    ${starPlaceholder}
+                    <div class="back-main-content d-flex justify-content-center align-items-center text-gradient">
+                        <div id="loaderB"></div>
+                        <div class="card-text m-4" id="pageB">${data.card.pageB}</div>
                     </div>
-                    <div style="width:40px" class="zobrazovat-jen-na-mobilu"></div>
-                    
-                    <div>
-                    <a href="/category/${data.card.category}/section/${data.card.section}/cardAjax/${data.nextNum}" class="btn btn-lg btn-danger easy-btn" id="btn-dalsi">Další</a>
-                    </div>
-                    <div>
-                        <a href="#" class="nezobrazovat-na-mobilu" id="rotate-back"><i class="fas fa-sync-alt fa-lg"></i></a>
-                        <a href="#" class="zobrazovat-jen-na-mobilu" id="rotate-back-mobile"><i class="fas fa-sync-alt fa-lg"></i></a>
-                    </div>
-                </div>
                 </div>
             </div>
             `);
 
-            $(".star-at-top").empty().append(`
-                ${starEmpty}
-                ${starFull}
-                ${starActivateModal}
-                ${starPlaceholder}
+            $("#front-menu-row").toggleClass('hide');
+            $("#back-menu-row").toggleClass('hide');
+            
+            $("#back-menu-row").empty().append(`
+            <div class="col-lg-6 offset-lg-2">
+                <div class="back-menu d-flex justify-content-between align-items-center border-grey">
+                <div class="">
+                    ${starEmpty}
+                    ${starFull}
+                    ${starActivateModal}
+                </div>
+                <div>
+                    <a href="/category/${data.card.category}/section/${data.card.section}/cardAjax/${data.nextNum}" class="btn btn-lg btn-danger easy-btn" id="btn-dalsi">Další</a>
+                </div>
+                <div>
+                    <a href="#" class="nezobrazovat-na-mobilu" id="rotate-back"><i class="fas fa-sync-alt fa-lg"></i></a>
+                    <a href="#" class="zobrazovat-jen-na-mobilu" id="rotate-back-mobile"><i class="fas fa-sync-alt fa-lg"></i></a>
+                </div>
+                </div>
+            </div>
             `)
+
+            
 
             //update progress bar
             $("#progressCardNumMobile").text(data.nextNum - 1);
@@ -100,13 +92,15 @@ $(document).ready(function() {
             $("#progressBarMac").css('width', data.progressStatus + "%");
             $("#predchozi-karticka-link").attr("name", `/category/${data.card.category}/section/${data.card.section}/cardAjax/${previousCard}`);
 
-            $("#flip-card #btn-otocit").click((e) => {
+            $("#front-menu-row #btn-otocit").click((e) => {
                 e.preventDefault();
                 $("#flip-card").toggleClass('flipped');
+                $("#back-menu-row").toggleClass('hide');
+                $("#front-menu-row").toggleClass('hide');
                 push++;
               })
 
-            $("#flip-card #btn-dalsi").click(function(e){
+            $("#back-menu-row #btn-dalsi").click(function(e){
                 e.preventDefault();
                 $(this).addClass('disabled')
                 $("#loaderB").append("<div class='spinner-border' role='status'><span class='visually-hidden'>Loading...</span></div>");
@@ -119,9 +113,11 @@ $(document).ready(function() {
                 }
             })
 
-            $("#flip-card #rotate-back").click((e) => {
+            $("#back-menu-row #rotate-back").click((e) => {
                 e.preventDefault();
                 $("#flip-card").toggleClass('flipped');
+                $("#back-menu-row").toggleClass('hide');
+                $("#front-menu-row").toggleClass('hide');
                 push--;
             })
 
@@ -133,6 +129,8 @@ $(document).ready(function() {
                     push++;
                     if(push === 1){
                         $("#flip-card").toggleClass('flipped');
+                        $("#back-menu-row").toggleClass('hide');
+                        $("#front-menu-row").toggleClass('hide');
                     }
                     if(push === 2){
                         $("#loaderB").append("<div class='spinner-border' role='status'><span class='visually-hidden'>Loading...</span></div>");
@@ -245,9 +243,11 @@ $(document).ready(function() {
         push--;
       });
 
-      $('#flip-card').on('click', '#rotate-back-mobile', function(e) {
+      $('#back-menu-row').on('click', '#rotate-back-mobile', function(e) {
         e.preventDefault(e);
         $("#flip-card").toggleClass('flipped');
+        $("#back-menu-row").toggleClass('hide');
+        $("#front-menu-row").toggleClass('hide');
         push--;
       });
 
@@ -268,6 +268,8 @@ $(document).ready(function() {
                 push++;
                 if(push === 1){
                     $("#flip-card").toggleClass('flipped');
+                    $("#back-menu-row").toggleClass('hide');
+                    $("#front-menu-row").toggleClass('hide');
                 }
                 if(push === 2){
                     $("#loaderB").append("<div class='spinner-border' role='status'><span class='visually-hidden'>Loading...</span></div>");
