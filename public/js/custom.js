fba0bec6d611a679a11e89fc8882eb3e5c87c32e
Jakub Spacil <jakubspacil@gmail.com>
2023-11-29 12:59:26 +0100

    verze 1.15

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-	$(".btn-spustit").click(function() {
-		$(this).addClass('disabled');
-	})
 
-	$(".btn-opakovat").click(function(e) {
-		console.log($(this));
-		$(this).addClass('disabled');
-	})
-
-	$(".btn-pokracovat").click(function(e) {
-		console.log($(this));
-		$(this).addClass('disabled');
-	})
 });
