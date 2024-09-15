14f2d84f237eb83feaad312e9e5129fd5a82f73c
Jakub Spacil <jakubspacil@gmail.com>
2024-09-15 14:19:59 +0300

    delete acount mobileApi logic

--- routes/mobileAuth.js
+++ routes/mobileAuth.js
@@ -188,4 +188,29 @@ router.get(
   })
 );
 
+router.delete(
+  "/mobileAuth/deleteAccount",
+  passport.authenticate("jwt", { session: false }),
+  catchAsync(async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "Uživatel neexistuje." });
+      }
+      if (user.plan !== "none") {
+        return res
+          .status(404)
+          .json({ message: "Před zrušením účtu je nutné zrušit přeplatné." });
+      }
+      await User.findByIdAndDelete(userId);
+      mail.adminInfoUserDeleted(user.email);
+      return res.status(200).json({ message: "Účet byl úspěšně smazán." });
+    } catch (error) {
+      console.log("Error deleting account:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  })
+);
+
 module.exports = router;
