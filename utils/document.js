ba5858b1c87335fe3505fe99cd0db406adfb2de6
Jakub Spacil <jakubspacil@gmail.com>
2024-12-20 11:03:05 +0100

    chunk spliting

---
+++ utils/document.js
@@ -0,0 +1,27 @@
+// utils.js
+function splitTextIntoChunks(text, maxTokens) {
+  console.log("Splitting text into chunks...");
+  const approxCharsPerToken = 4; // Rough estimate
+  const maxChunkSize = maxTokens * approxCharsPerToken;
+
+  const chunks = [];
+  let currentChunk = "";
+
+  const sentences = text.split(/(?<=[.!?])\s+/);
+
+  for (const sentence of sentences) {
+    if ((currentChunk + sentence).length > maxChunkSize) {
+      chunks.push(currentChunk);
+      currentChunk = "";
+    }
+    currentChunk += sentence + " ";
+  }
+
+  if (currentChunk) {
+    chunks.push(currentChunk);
+  }
+
+  return chunks;
+}
+
+module.exports = { splitTextIntoChunks };
