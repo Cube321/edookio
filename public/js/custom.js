dc95c3c305bbfcc8bcf86b7688da60fe324341b7
cube321 <jakubspacil@gmail.com>
2025-01-14 17:44:30 +0100

    removed feedback for packages and hiden stars rating

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
   let ratingSent = false;
   $(".radio-btn-rating").click(function () {
     let selectedValue = $("input[name='rate']:checked").val();
-    $("#text-feedback-link").removeClass("hidden");
     //send data to db
     let sectionId = $("#rating-form").attr("name");
     let updateRatingUrl = `/api/updateRating/section/${sectionId}/${selectedValue}?type=${type}`;
@@ -14,23 +13,6 @@ $(document).ready(function () {
         url: `${updateRatingUrl}`,
       }).catch((err) => console.log(err));
     }
-  });
-
-  $("#submit-text-feedback").click(function () {
-    let text = $("#feedback-textarea").val();
-    //send data to db
-    let sectionId = $("#rating-form").attr("name");
-    let saveFeedbackUrl = `/api/saveFeedback/section/${sectionId}?type=${type}`;
-    $.ajax({
-      method: "POST",
-      url: `${saveFeedbackUrl}`,
-      data: {
-        text,
-      },
-    }).catch((err) => console.log(err));
-
-    $(".modal-body").empty();
-    $(".modal-body").append("<p>Odesláno, díky!</p>");
   });
 
   if ($("#successOverlay")) {
