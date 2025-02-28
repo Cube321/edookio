102ec0922eb4c5d18df6fb4fde8b7c8ddcf4fbe8
cube321 <jakubspacil@gmail.com>
2025-02-28 15:27:53 +0100

    animation on landing A

--- public/js/landing.js
+++ public/js/landing.js
@@ -9,12 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
   heading.classList.add("animate");
   lead.classList.add("animate");
   callBtn.classList.add("animate");
-  callBtn2.classList.add("animate");
 
-  $("#add-category-btn-on-home").click(function () {
-    $(this).addClass("disabled");
-    $(this).empty();
-    $(this).append(
+  if (callBtn2) {
+    callBtn2.classList.add("animate");
+  }
+
+  $("#add-category-form").submit(function () {
+    $("#add-category-btn-on-home").addClass("disabled");
+    $("#add-category-btn-on-home").empty();
+    $("#add-category-btn-on-home").append(
       "<div class='spinner-border spinner-border-small' role='status'><span class='visually-hidden'>Loading...</span></div>"
     );
   });
